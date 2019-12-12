a6541c59f7e7c46389576cd4f22addcf6123ef39
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2019-12-12 10:13:04 -0600

    js not wokring, smoothscroll work

--- js/main.js
+++ js/main.js
@@ -1,40 +1,41 @@
 // Smooth scroll
-$('#about').on('click', function () {
-    const programs = $('#programs').position().top;
+// $(document).ready(function () {
+//             $('#about').on('click', function () {
+//                 const programs = $('#programs').position().top;
 
-    $('html, body').animate({
-        scrollTop: programs
-    }, 900);
-});
+//                 $('html, body').animate({
+//                     scrollTop: programs
+//                 }, 900);
+//             });
+//         }
+        // $('#view-food').on('click', function () {
+        //     const images = $('#food-images').position().top;
 
-$('#view-food').on('click', function () {
-    const images = $('#food-images').position().top;
+        //     $('html, body').animate({
+        //         scrollTop: images
+        //     }, 1000);
+        // });
 
-    $('html, body').animate({
-        scrollTop: images
-    }, 1000);
-});
+        // $('#contact-us').on('click', function () {
+        //     const landing = $('#landing').position().top - 30;
 
-$('#contact-us').on('click', function () {
-    const landing = $('#landing').position().top - 30;
+        //     $('html, body').animate({
+        //         scrollTop: landing
+        //     }, 1000);
+        // });
 
-    $('html, body').animate({
-        scrollTop: landing
-    }, 1000);
-});
+        // $('#view-team-banner').on('click', function () {
+        //     const images = $('#team-images').position().top;
 
-$('#view-team-banner').on('click', function () {
-    const images = $('#team-images').position().top;
+        //     $('html, body').animate({
+        //         scrollTop: images
+        //     }, 900);
+        // });
 
-    $('html, body').animate({
-        scrollTop: images
-    }, 900);
-});
+        // $('#view-food-banner').on('click', function () {
+        //     const images = $('#food-images').position().top;
 
-$('#view-food-banner').on('click', function () {
-    const images = $('#food-images').position().top;
-
-    $('html, body').animate({
-        scrollTop: images
-    }, 1000);
-});
+        //     $('html, body').animate({
+        //         scrollTop: images
+        //     }, 1000);
+        // });

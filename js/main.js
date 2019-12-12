8fef7dcb7c30dfb86da4b5a3f45b17f236eb3913
Valentin Cantu <MeroMero@Valentins-MacBook-Pro.local>
2019-12-11 21:37:46 -0600

    js included

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Smooth scroll
+$('#about').on('click', function () {
+    const programs = $('#programs').position().top;
+
+    $('html, body').animate({
+        scrollTop: programs
+    }, 900);
+});
+
+$('#view-food').on('click', function () {
+    const images = $('#food-images').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 1000);
+});
+
+$('#contact-us').on('click', function () {
+    const landing = $('#landing').position().top - 30;
+
+    $('html, body').animate({
+        scrollTop: landing
+    }, 1000);
+});
+
+$('#view-team-banner').on('click', function () {
+    const images = $('#team-images').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 900);
+});
+
+$('#view-food-banner').on('click', function () {
+    const images = $('#food-images').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 1000);
+});
